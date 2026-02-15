a740d1c0dbf9c3bdbdc3a9a9a065894abb8338f4
prvmeenu <Prvmeenu@gmail.com>
2026-02-15 01:09:28 +0100

    Add files via upload

---
+++ EmotionDetection/emotion_detection.py
@@ -0,0 +1,28 @@
+import requests
+import json
+
+def emotion_detector(text_to_analyze):
+    url ='https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
+    Headers = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
+    myobj = { "raw_document": { "text": text_to_analyze } }
+    response = requests.post(url, json = myobj, headers = Headers)
+    formatted_response = json.loads(response.text)
+
+    if response.status_code == 200:
+        anger = formatted_response['emotionPredictions'][0]['emotion']['anger']
+        disgust = formatted_response['emotionPredictions'][0]['emotion']['disgust']
+        fear = formatted_response['emotionPredictions'][0]['emotion']['fear']
+        joy = formatted_response['emotionPredictions'][0]['emotion']['joy']
+        sadness = formatted_response['emotionPredictions'][0]['emotion']['sadness']
+        emotion = {'anger' : anger, 'disgust': disgust, 'fear': fear, 'joy' : joy, 'sadness': sadness}
+        dominant_emotion = max(emotion, key = emotion.get)
+    elif response.status_code == 400:
+        anger = None
+        disgust = None
+        fear = None
+        joy = None
+        sadness = None
+        dominant_emotion = None
+
+    return {'anger' : anger, 'disgust': disgust, 'fear': fear, 'joy' : joy, 'sadness': sadness, 'dominant_emotion':dominant_emotion}
+    
